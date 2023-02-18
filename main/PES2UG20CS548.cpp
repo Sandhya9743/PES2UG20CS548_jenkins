1615539664a2c40cc0c7bbe7f5d534dbcb7ce417
Sandhya9743 <115204085+Sandhya9743@users.noreply.github.com>
2023-02-18 20:43:09 +0530

    Update PES2UG20CS548.cpp

--- main/PES2UG20CS548.cpp
+++ main/PES2UG20CS548.cpp
@@ -1,10 +1,7 @@
-//File: hello.cpp
 #include <iostream>
-using namespace std;
 
-int main(void) {
-  cout << "good morning, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+int main() {
+  std::cout << "Hello, World!\n";
   return 0;
 }
+
